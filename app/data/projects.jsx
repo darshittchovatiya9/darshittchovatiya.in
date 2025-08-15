5a5c63d7cb49129d51e206b6e7c8339dc751b99c
Darshit-jbs-07 <darshit.jbs07@gmail.com>
2025-08-15 14:44:31 +0530

    - project add component

--- app/data/projects.jsx
+++ app/data/projects.jsx
@@ -1,34 +1,22 @@
-import Tracker from "../resources/images/project/tracker.png";
 import Trello from "../resources/images/project/trello.png";
-// import SQL from "../resources/images/project/sql.png";
-import Librarium from "../resources/images/project/librarium.png";
-import Git from "../resources/images/project/git.png";
-import Course from "../resources/images/project/course.png";
-import jukebox from "../resources/images/project/jukebox.png";
-import IdeaTracker from "../resources/images/project/idea.png";
+
+import ECommerceAdmin from "../resources/images/project/e-commerce-admin.png";
+import ECommerceWebsite from "../resources/images/project/e-commerce-website.png";
 
 const projects = [
   {
-    image: IdeaTracker,
-    title: "Idea Tracker",
+    image: ECommerceAdmin,
+    title: "E Commerce Admin",
     description:
-      "A React + Appwrite app to save, search, and manage side project ideas. Features auth, real-time updates, and a clean UI with Tailwind & Framer Motion.",
-    url: "https://idea-tracker.appwrite.network/",
-    githubLink: "https://github.com/AbhiVarde/Idea-tracker",
+      "A robust and scalable admin dashboard designed to streamline e-commerce operations. This project provides a user-friendly interface for managing products, categories, orders, and store settings while offering advanced features like analytics, image handling, and secure authentication.",
+    githubLink: "https://github.com/darshit15-02/E-commerce-AdminPanel",
   },
   {
-    image: Librarium,
-    title: "Librarium",
+    image: ECommerceWebsite,
+    title: "E Commerce Website",
     description:
-      "A full-stack solution with Google and Discord login, powerful search, and complete CRUD operations for seamless book management.",
-    githubLink: "https://github.com/AbhiVarde/Librarium",
-  },
-  {
-    image: Tracker,
-    title: "SpaceX Launch Tracker",
-    description:
-      "A full-stack web application for tracking SpaceX launches, offering real-time updates on launch information and status.",
-    githubLink: "https://github.com/AbhiVarde/SpaceX-Launch-Tracker",
+      "SPARKLECART is a modern and secure e-commerce platform designed to provide a seamless shopping experience. This project leverages cutting-edge technologies to deliver a user-friendly, scalable, and performance-driven solution.",
+    githubLink: "https://github.com/darshit15-02/E-commerce-Website",
   },
   {
     image: Trello,
@@ -37,38 +25,6 @@ const projects = [
       "Facilitating collaborative task management in real-time, enabling users to create boards, lists, and cards for efficient organization and teamwork.",
     url: "https://trello-bice.vercel.app/",
     githubLink: "https://github.com/AbhiVarde/Trello-Clone",
-  },
-  // {
-  //   image: SQL,
-  //   title: "SQL AI",
-  //   description:
-  //     "Creating a web app allowing users to input SQL queries, generating responses using ChatGPT plugin.",
-  //   url: "https://sqlai.vercel.app/",
-  //   githubLink: "https://github.com/AbhiVarde/SQL-AI",
-  // },
-  {
-    image: jukebox,
-    title: "JukeBox",
-    description:
-      "JukeBox is a web application facilitating Spotify login, search, and playback functionality for users.",
-    url: "https://jukebox-xi.vercel.app/",
-    githubLink: "https://github.com/AbhiVarde/Jukebox",
-  },
-  {
-    image: Git,
-    title: "Git Wars",
-    description:
-      "An interactive platform enabling users to enhance Git proficiency through task completion, aiming for mastery in version control.",
-    url: "https://gitwars.vercel.app/",
-    githubLink: "https://github.com/AbhiVarde/Git-Wars",
-  },
-  {
-    image: Course,
-    title: "CourseExpert",
-    description:
-      "An online platform enables users to log in/out, add courses for sale, and purchase available courses.",
-    url: "https://courseexpert.vercel.app/",
-    githubLink: "https://github.com/AbhiVarde/CourseExpert",
   },
 ];
 
