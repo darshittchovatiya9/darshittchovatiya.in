10e3bfa5a7b00ec6586c4cd27bbe596b13ea0086
darshit15-02 <darshitc18@gmail.com>
2025-08-15 15:58:59 +0530

    - project some changed

--- app/data/projects.jsx
+++ app/data/projects.jsx
@@ -2,30 +2,54 @@ import Trello from "../resources/images/project/trello.png";
 
 import ECommerceAdmin from "../resources/images/project/e-commerce-admin.png";
 import ECommerceWebsite from "../resources/images/project/e-commerce-website.png";
+import MERNStackAuthentication from "../resources/images/project/mern-stack-authentication-system.png";
+import Hotstar from "../resources/images/project/hotstar.png";
+import IgInsta from "../resources/images/project/IgInsta.png";
 
 const projects = [
   {
     image: ECommerceAdmin,
     title: "E Commerce Admin",
     description:
-      "A robust and scalable admin dashboard designed to streamline e-commerce operations. This project provides a user-friendly interface for managing products, categories, orders, and store settings while offering advanced features like analytics, image handling, and secure authentication.",
+      "A scalable e-commerce admin dashboard with a user-friendly interface for managing products, categories, orders, and store settings, featuring analytics, image handling, and secure authentication.",
     githubLink: "https://github.com/darshit15-02/E-commerce-AdminPanel",
   },
   {
     image: ECommerceWebsite,
     title: "E Commerce Website",
     description:
-      "SPARKLECART is a modern and secure e-commerce platform designed to provide a seamless shopping experience. This project leverages cutting-edge technologies to deliver a user-friendly, scalable, and performance-driven solution.",
+      "SPARKLECART is a modern, secure e-commerce platform built with advanced technologies for a seamless, scalable, and high-performance shopping experience.",
     githubLink: "https://github.com/darshit15-02/E-commerce-Website",
   },
   {
-    image: Trello,
-    title: "Trello Clone",
+    image: MERNStackAuthentication,
+    title: "Building a Secure MERN Stack Authentication System 🚀",
     description:
-      "Facilitating collaborative task management in real-time, enabling users to create boards, lists, and cards for efficient organization and teamwork.",
-    url: "https://trello-bice.vercel.app/",
-    githubLink: "https://github.com/AbhiVarde/Trello-Clone",
+      "A secure MERN stack authentication system with JWT-based login, registration, email verification, and OTP password reset.",
+    githubLink: "https://github.com/darshit15-02/Mern_Auth",
   },
+  {
+    image: Hotstar,
+    title: "Disney+ Hotstar Clone",
+    description:
+      "Developed a Disney+ Hotstar clone, replicating its core streaming functionalities. Features include user authentication, content browsing, and video playback with a responsive UI.",
+    githubLink: "https://github.com/darshit15-02/Disney-Hotstar",
+  },
+  {
+    image: IgInsta,
+    title: "Instagram video Dowloader",
+    description:
+      "IGinsta.in is a fast, user-friendly platform for downloading Instagram videos, images, stories, and posts. Enjoy high-speed downloads, no sign-up, and easy navigation for a seamless experience.",
+    url: "https://iginsta.in/",
+  }
+  // {
+  //   image: Trello,
+  //   title: "Trello Clone",
+  //   description:
+  //     "Facilitating collaborative task management in real-time, enabling users to create boards, lists, and cards for efficient organization and teamwork.",
+  //   url: "https://trello-bice.vercel.app/",
+  //   githubLink: "https://github.com/AbhiVarde/Trello-Clone",
+  // },
 ];
 
 export default projects;
