0b03d846fe2d7b2888299b7a448e252712042a60
darshit15-02 <darshitc18@gmail.com>
2025-08-18 13:22:39 +0530

    - mere images changes name

--- app/data/blogs.jsx
+++ app/data/blogs.jsx
@@ -1,8 +1,8 @@
-import Mern from "../resources/images/blog/Mern.png";   
+import MernImage from "../resources/images/blog/Mern.png";   
 
 const blogs = [
   {
-    image: Mern,
+    image: MernImage,
     title: "🚀 Building a Secure MERN Stack Authentication System with JWT, Email Verification, and OTP-Based Password Reset",
     desc: "A secure MERN stack authentication system with JWT-based login, registration, email verification, and OTP password reset. Features MongoDB, Express.js, React with Tailwind CSS, and secure APIs.",
     url: "https://medium.com/@darshit.patel07/building-a-secure-mern-stack-authentication-system-with-jwt-email-verification-and-otp-based-bafa828d0eb1",
