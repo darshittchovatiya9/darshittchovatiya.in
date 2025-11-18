e379f73f26e0db28bab600366bf8b3c292cb599d
Darshitt Chovatiya <127707401+darshittchovatiya9@users.noreply.github.com>
2025-11-18 14:37:40 +0530

    Update social links with new URLs

--- app/data/socialLinks.jsx
+++ app/data/socialLinks.jsx
@@ -6,25 +6,25 @@ const socialLinks = [
   {
     id: "github",
     name: "Github",
-    url: "https://github.com/darshitchovatiya",
+    url: "https://github.com/darshittchovatiya9",
     icon: <LuGithub size={20} color="#FFFFFF" />,
   },
   {
     id: "linkedin",
     name: "LinkedIn",
-    url: "http://www.linkedin.com/in/darshit-chovatiya07",
+    url: "www.linkedin.com/in/darshittchovatiya9",
     icon: <FaLinkedinIn size={20} color="#FFFFFF" />,
   },
   {
     id: "instagram",
     name: "Instagram",
-    url: "https://www.instagram.com/_darshit_07_",
+    url: "https://www.instagram.com/_darshitt_9_",
     icon: <FaInstagram  size={20} color="#FFFFFF" />,
   },
   {
     id: "medium",
     name: "Medium",
-    url: "https://medium.com/@darshit.chovatiya07",
+    url: "https://medium.com/@darshitt.chovatiya9",
     icon: <FaMedium size={20} color="#FFFFFF" />,
   }
 ];
